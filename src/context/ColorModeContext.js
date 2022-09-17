6abf22155de593b9e7294fd634ff365b2709840b
neuralchemist <hacker.turab91@gmail.com>
2022-09-17 21:33:09 +0200

    (refactor) using default mui light and dark theme

--- src/context/ColorModeContext.js
+++ src/context/ColorModeContext.js
@@ -1,7 +1,6 @@
 // colorModeContext.js
 import { useState, useMemo, createContext, useContext } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { deepPurple, amber, teal, grey,  brown,  } from "@mui/material/colors";
 
 
 const ColorModeContext = createContext({
@@ -26,44 +25,13 @@ export function ColorModeProvider({ children }) {
     [mode]
   );
 
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-          ...(mode === "light"
-            ? {
-                // palette values for LIGHT MODE
-                primary: deepPurple,
-                secondary: teal,
-                // divider: deepPurple[200],
-                background: {
-                  default: brown[200],
-                  paper: brown[300],
-                },
-                text: {
-                  primary: grey[900],
-                  secondary: grey[800],
-                },
-              }
-            : {
-                // palette values for DARK MODE
-                primary: amber,
-                // divider: amber[700],
-                divider: brown,
-                background: {
-                  default: grey[800],
-                  paper: grey[900],
-                },
-                text: {
-                  primary: grey[50],
-                  secondary: grey[100]
-                },
-              }),
-        },
-      }),
-    [mode]
-  );
+  const theme =  useMemo(() => createTheme({
+    palette: {
+      mode: mode
+    }
+
+  }), [mode])
+
 
   return (
     <ColorModeContext.Provider value={colorMode}>
